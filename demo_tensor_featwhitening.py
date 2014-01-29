d791d9a1861790bb4dd9d5e8081220d7b153d212
Erik Rodner <Erik.Rodner@uni-jena.de>
2014-01-30 00:26:47 +0100

    start of the tensor impl.

---
+++ demo_tensor_featwhitening.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pylab
+import sys
+from timer import Timer
+from featwhitening import FeatureWhitening
+
+
+####### MAIN
+
+if len(sys.argv)<2:
+    imgfn = "lena.jpg"
+else:
+    imgfn = sys.argv[1]
+
+imgraw = pylab.imread(imgfn)
+
+#img = (imgraw - np.mean(imgraw,axis=None)) / 255.0
+img = imgraw
+
+ps = 3
+patchSize = np.array([ps,ps])
+
+np.set_printoptions( precision = 4 )
+
+fw = FeatureWhitening() 
+with Timer('FFT Patch Correlation') as t:
+  C_eff = fw.getPatchCorrelation(img,patchSize)
+
+if np.prod(patchSize) < 10:
+    print "correlation matrix (efficient)"
+    print C_eff
+
+wimg_eff = fw.whitenImage(img)
+print "Correlation matrix after whitening:"
+C = fw.getPatchCorrelation( wimg_eff, patchSize )
+print C
+
+pylab.imshow( wimg_eff, cmap=pylab.cm.gray )
+pylab.title('Image Whitening')
+pylab.show()
--- featwhitening.py
+++ featwhitening.py
@@ -13,6 +13,7 @@ from timer import Timer
 """
 class FeatureWhitening:
 
+    """ Whiten the image completely and directly """
     def whitenImage(self,img,patchSize=None):
         ft = np.fft.fft2(img)
         powerspec_sqrt = np.sqrt ( np.multiply( np.conjugate(ft), ft ) )
@@ -30,47 +31,108 @@ class FeatureWhitening:
         
         return wimg * np.sqrt( np.prod(img.shape) )
 
+    """ Compute the power spectrum of an image """
     def computePowerSpec(self,img):
-        ft = np.fft.fft2(img)
-        powerspec = np.multiply( np.conjugate(ft), ft )
-        A = np.fft.fftshift( np.fft.ifft2(powerspec) )
+        if img.ndim==2:
+            # only one plane
+            ft = np.fft.fft2(img)
+            powerspec = np.multiply( np.conjugate(ft), ft )
+            A = np.fft.fftshift( np.fft.ifft2(powerspec) )
+        else:
+            # more than one plane
+            ft = np.fft.fftn(img)
+            powerspec = np.multiply( np.conjugate(ft), ft )
+            A = np.fft.fftshift( np.fft.ifftn(powerspec) )
+            
         return A
 
+
+    """ Compute the correlation matrix of the patch """
     def getPatchCorrelation(self,img,patchSize):
         
         #with Timer('Calculation of auto-correlation'):
         A = self.computePowerSpec(img)
             
-        n = np.prod(patchSize)
-        
-        # build a block Toeplitz matrix from the auto-correlation results
-        #with Timer('Building C'):
-        yoff, xoff = np.meshgrid( range(patchSize[0]), range(patchSize[1]), indexing='ij' )
-        xoff = np.reshape( xoff, n )
-        yoff = np.reshape( yoff, n )
         my = A.shape[0]/2
         mx = A.shape[1]/2
 
-        #with Timer('Non-vectorized version'):
-        #  C = np.zeros([n,n])
-        #  # non-vectorized version
-        #  for i1 in range(n):
-        #    C[i1,i1] = np.real( A[m1,m2] )
-        #    for i2 in range(i1+1,n):
-        #      xd = xoff[i1] - xoff[i2]
-        #      yd = yoff[i1] - yoff[i2]
-        #      C[i1,i2] = np.real(A[m2+xd,m1+yd])
-        #      C[i2,i1] = C[i1,i2]
-        #with Timer('Vectorized version'):
+        # build a block Toeplitz matrix from the auto-correlation results
+        if A.ndim==2:
+            # single plane only
+            n = np.prod(patchSize)
 
-        # vectorized version
-        e = np.ones([n,1])
-        xd = np.outer( xoff, e.T ) - np.outer( e, xoff.T )
-        xd = np.reshape ( xd+mx, n*n )
-        yd = np.outer( yoff, e.T ) - np.outer( e, yoff.T )
-        yd = np.reshape ( yd+my, n*n )
-        C = np.reshape ( np.real(A[np.int_(yd),np.int_(xd)]), [n, n] )
+            #with Timer('Building C'):
+            yoff, xoff = np.meshgrid( range(patchSize[0]), range(patchSize[1]), indexing='ij' )
+            xoff = np.reshape( xoff, n )
+            yoff = np.reshape( yoff, n )
 
+            # vectorized version of the below written non-vectorized version
+            e = np.ones([n,1])
+            xd = np.outer( xoff, e.T ) - np.outer( e, xoff.T )
+            xd = np.reshape ( xd+mx, n*n )
+            yd = np.outer( yoff, e.T ) - np.outer( e, yoff.T )
+            yd = np.reshape ( yd+my, n*n )
+            C = np.reshape ( np.real(A[np.int_(yd),np.int_(xd)]), [n, n] )
+
+            #with Timer('Non-vectorized version'):
+            #  C = np.zeros([n,n])
+            #  # non-vectorized version
+            #  for i1 in range(n):
+            #    C[i1,i1] = np.real( A[mx,my] )
+            #    for i2 in range(i1+1,n):
+            #      xd = xoff[i1] - xoff[i2]
+            #      yd = yoff[i1] - yoff[i2]
+            #      C[i1,i2] = np.real(A[mx+xd,my+yd])
+            #      C[i2,i1] = C[i1,i2]
+        
+        else:
+            # multiple plane case
+            n = np.prod(patchSize) * A.shape[2]
+
+            print "This part of the code is still under development"
+            
+            yoff, xoff, zoff = np.meshgrid( range(patchSize[0]), range(patchSize[1]), range(A.shape[2]), indexing='ij' )
+            xoff = np.reshape( xoff, n )
+            yoff = np.reshape( yoff, n )
+            zoff = np.reshape( zoff, n )
+            print xoff
+            print yoff
+            print zoff
+
+            mz = A.shape[2]/2
+
+            C = np.zeros([n,n])
+            
+            # vectorized version of the below written non-vectorized version
+            e = np.ones([n,1])
+            xd = np.outer( xoff, e.T ) - np.outer( e, xoff.T )
+            print xd
+            xd = np.reshape ( xd+mx, n*n )
+            yd = np.outer( yoff, e.T ) - np.outer( e, yoff.T )
+            print yd
+            yd = np.reshape ( yd+my, n*n )
+            zd = np.outer( zoff, e.T ) - np.outer( e, zoff.T )
+            print zd
+            zd = np.reshape ( zd+mz, n*n )
+
+            C = np.reshape ( np.real(A[np.int_(yd),np.int_(xd),np.int_(zd)]), [n, n] )
+
+
+            # non-vectorized version
+            #for i1 in range(n):
+            #    C[i1,i1] = np.real( A[mx,my,mz] )
+            #    for i2 in range(i1+1,n):
+            #        xd = xoff[i1] - xoff[i2]
+            #        yd = yoff[i1] - yoff[i2]
+            #        zd = zoff[i1] - zoff[i2]
+            #        print zd
+            #        print mz
+            #        C[i1,i2] = np.real(A[mx+xd,my+yd,mz+zd])
+            #        C[i2,i1] = C[i1,i2]
+                     
+
+
+            
         # return a block Toeplitz matrix
         C = C / np.prod(A.shape)
         return C
